4cdc9e07eb3c6fc397c52f577d1a3df7022f0e17
ShadyF <shadster09@gmail.com>
2017-01-17 14:38:28 +0200

    Added placeholder blog articles to homepage

--- src/components/views/app/app.js
+++ src/components/views/app/app.js
@@ -7,13 +7,37 @@ import logo from './csealogo.png'
 
 export default class App extends Component {
     render() {
+        const article = (<article className={styles.post}>
+            <header>
+                <h2 className={styles.title}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </h2>
+                <h5 className={styles.subtitle}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </h5>
+            </header>
+            <span>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at vestibulum nunc.
+                                        Proin tincidunt ex sapien, maximus tristique ligula finibus sit amet. Quisque eu massa mollis, egestas ipsum vel, molestie magna.
+                                        Donec porttitor ac turpis non pharetra. Duis vitae interdum orci. Morbi nisl tellus, aliquam vel lobortis id, maximus at purus.
+                                        Nam rutrum leo lectus, vel dictum velit luctus quis. Donec id eleifend risus. Aliquam erat volutpat. Maecenas ac nibh diam. In non cursus felis.
+                                    </span>
+            <footer className={styles.postMeta}>
+                <span className={styles.postAuthor}>CSEA Admin</span>
+                <span className={styles.postDate}> 15 SEPTEMBER 2015</span>
+                <span className={styles.postCommentCount}>5 Comments</span>
+                <span className={styles.postContinueReading}>Continue Reading</span>
+            </footer>
+        </article>);
+
         return (
             <div className="appWrapper">
                 <CSEANavbar fixedTop fluid collapseOnSelect inverse/>
                 <section className={"container-fluid " + styles.banner}>
                     <header>
                         <h4>The American University In Cairo's</h4>
-                        <h2>Computer Science and Engineering Association</h2>
+                        <h1>Computer Science and Engineering Association</h1>
                     </header>
                     <div className={styles.logoWrapper}>
                         <Image className={styles.CSEALogo} src={logo} circle responsive/>
@@ -22,8 +46,10 @@ export default class App extends Component {
                 <section className={styles.mainWrapper}>
                     <Grid fluid>
                         <Row>
-                            <Col className={styles.main} md={8} mdOffset={2}>
-                                {this.props.children}
+                            <Col className={styles.main} md={6} mdOffset={3}>
+                                {article}
+                                {article}
+                                {article}
                             </Col>
                         </Row>
                     </Grid>
