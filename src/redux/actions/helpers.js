8413fc94b4451bd551b9d01e90266f65b3c544eb
shadyf <shadster09@gmail.com>
2017-01-20 20:27:06 +0200

    Fixed incorrect use of string literals in actions/helpers

--- src/redux/actions/helpers.js
+++ src/redux/actions/helpers.js
@@ -4,7 +4,7 @@ const FAILURE = 'FAILURE';
 
 function createRequestTypes(base) {
     return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
-        acc[type] = '${base}_${type}';
+        acc[type] = `${base}_${type}`;
         return acc
     }, {})
 }
