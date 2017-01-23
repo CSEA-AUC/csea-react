97ea4c97de966e05aafa06b9060f0cd0b336c0ad
shadyf <shadster09@gmail.com>
2017-01-23 02:47:07 +0200

    Implemented first usage of a saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,24 +1,34 @@
-import {take, put, call, fork, select} from 'redux-saga/effects'
+import {takeEvery, put, call, fork, select} from 'redux-saga/effects'
 import * as actions from '../actions'
+import * as api from '../services/api'
 
 // Subroutines
-
-function* loadPostList(url, pageNum) {
-    "use strict";
-
+// TODO: Check if posts were cached or not in subroutines
+// use select to get the state of the app
+// See http://stackoverflow.com/questions/38405700/getstate-in-redux-saga
+function* loadPostList(action) {
+    const {pageNum} = action;
+    try {
+        const apiData = yield call(api.fetchResource, 'blog/posts/');
+        console.log(apiData)
+    }
+    catch (error) {
+        // console.log(error.response.data);
+    }
 }
 
 // Watchers
-function* watchLoadPostsList() {
-    while (true) {
-        const what = yield take(actions.LOAD_POST_LIST);
-        yield console.log(what);
-    }
+function* watchLoadPostList() {
+    // while (true) {
+    //     const {pageNum} = yield take(actions.LOAD_POST_LIST);
+    //     yield fork(loadPostList, pageNum);
+    // }
+    yield takeEvery(actions.LOAD_POST_LIST, loadPostList);
 }
 
 // Root saga
 export default function* rootSaga() {
     yield [
-        fork(watchLoadPostsList)
+        fork(watchLoadPostList)
     ]
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,8 @@
-const API_ROOT = 'localhost:8000/';
+const API_ROOT = 'http://localhost:8000/';
+import Axios from 'axios'
 
+export function fetchResource(endpoint) {
+    const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
+
+    return Axios.get(fullUrl)
+}
