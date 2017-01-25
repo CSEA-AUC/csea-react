2939613733a7e74e9234285783b4c659d773495a
shadyf <shadster09@gmail.com>
2017-01-26 00:28:42 +0200

    Added ViewPost container to containers' index

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,8 @@
-export {default as App} from './app/app';
-export {default as Home} from './home/home';
-export {default as About} from './about/about';
+export {default as App} from './app/app'
+export {default as Home} from './home/home'
+export {default as About} from './about/about'
 
 export {default as Announcements} from './announcements/announcements'
 export {default as ListPosts} from './list-posts/list-posts'
+export {default as ViewPost} from './view-post/view-post'
 
