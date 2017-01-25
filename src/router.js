6cced38c35ac17e92ba9c41f7e87809813944b98
shadyf <shadster09@gmail.com>
2017-01-26 00:47:51 +0200

    react-router-redux now properly works

--- src/router.js
+++ src/router.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import {Router, Route, browserHistory, hashHistory, IndexRoute}from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+
+import store from './store'
 
 import {
     App,
     Home,
     About,
     Announcements,
-    ListPosts
+    ListPosts,
+    ViewPost
 } from './containers'
 
+
+const history = syncHistoryWithStore(hashHistory, store);
+
 export default (
-    <Router history={hashHistory}>
+    <Router history={history}>
         <Route path="/" component={App}>
             <IndexRoute component={Home}/>
             <Route path="about" component={About}/>
             <Route path="announcements" component={Announcements}>
                 <IndexRoute component={ListPosts}/>
+                <Route path=":slug" component={ViewPost}/>
             </Route>
             {/*<Route path="*" status={404}/>*/}
         </Route>
