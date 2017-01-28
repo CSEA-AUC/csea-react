07c4b812bfd311597fc088be499aa337417af8fd
shadyf <shadster09@gmail.com>
2017-01-28 12:53:06 +0200

    Added initial jsx for footer

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,8 +1,9 @@
 import React, {Component, PropTypes} from 'react'
-import {Nav, NavItem, Navbar} from 'react-bootstrap'
+import {Nav, NavItem, Navbar, Grid, Row, Col} from 'react-bootstrap'
 import {Link} from 'react-router'
 import {LinkContainer} from 'react-router-bootstrap'
-
+import Envelope from 'react-icons/lib/fa/envelope'
+import Facebook from 'react-icons/lib/fa/facebook'
 import styles from './app.scss'
 
 export default class App extends Component {
@@ -26,6 +27,25 @@ export default class App extends Component {
                 </Navbar>
                 {this.props.children}
                 <footer className={styles.appFooter}>
+                    <Grid>
+                        <Row>
+                            <Col className={styles.linksWrapper} xs={6}>
+                                <nav className={styles.navIcons}>
+                                    <Facebook className={styles.facebookIcon}/>
+                                    <Envelope className={styles.mailIcon}/>
+                                </nav>
+                                <nav className={styles.cseaLinks}>
+                                    <strong>CSEA</strong>
+                                    <Link to={'/announcements'}>Announcements</Link>
+                                    <Link to={'/notes'}>Notes</Link>
+                                    <Link to={'/about'}>About</Link>
+                                    <Link to={'/team'}>Team</Link>
+                                </nav>
+                            </Col>
+                            <Col className={styles.copyrightWrapper} xs={6}>
+                            </Col>
+                        </Row>
+                    </Grid>
                 </footer>
             </div>
         )
