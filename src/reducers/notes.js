fe9ed16fce05c2bfb6f6f61c04691f1d4fb2eadd
shadyf <shadster09@gmail.com>
2017-02-10 15:01:11 +0200

    Changed notes reducer to properly handle new notes actions

--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -2,31 +2,74 @@ import * as types from '../actions/notes'
 
 // Change currentNotes to notesbycourse
 const initialState = {
-    notesByCourse: {
-        isFetching: true,
-        responseCode: null
-    },
+    notesByCourse: {},
     selectedCourse: '',
-    availableCourses: []
+    availableCourses: {
+        isFetching: true,
+        responseCode: null,
+        results: []
+    }
 };
 
 function notes(state = initialState, action) {
     switch (action.type) {
-        case types.NOTES.REQUEST:
-            return {...state, currentNotes: {isFetching: true, responseCode: null}};
-        case types.NOTES.SUCCESS:
-            return {
-                ...state,
-                currentNotes: {results: action.notes, isFetching: false, responseCode: action.responseCode}
-            };
-        case types.NOTES.FAILURE:
-            return {...state, currentNotes: {isFetching: false, responseCode: action.errorCode}};
         case types.SELECT_COURSE:
-            return {...state, selectedCourse: action.courseName};
-        case types.LOAD_AVAIL_COURSES:
-            return {...state, availableCourses: action.courses}
+            return {...state, notesByCourse: action.courseName};
+
+        case types.NOTES.REQUEST:
+        case types.NOTES.SUCCESS:
+        case types.NOTES.FAILURE:
+            return {...state, notesByCourse: notesByCourseReducer(state.notesByCourse, action)};
+
+        case types.COURSES.REQUEST:
+        case types.COURSES.SUCCESS:
+        case types.COURSES.FAILURE:
+            return {...state, availableCourses: availableCoursesReducer(state.availableCourses, action)}
     }
     return state
+}
+
+function notesByCourseReducer(state=initialState, action) {
+    switch(action.type) {
+        case types.NOTES.REQUEST:
+            return {...state, [action.courseName]: {
+                isFetching: true,
+                responseCode: null
+            }};
+        case types.NOTES.SUCCESS:
+            return {...state, [action.courseName]: {
+                isFetching: false,
+                responseCode: action.responseCode,
+                results: action.notes
+            }};
+        case types.NOTES.FAILURE:
+            return {...state, [action.courseName]: {
+                isFetching: false,
+                responseCode: action.errorCode,
+            }}
+    }
+}
+function availableCoursesReducer(state = initialState, action) {
+    switch (action.type) {
+        case types.COURSES.REQUEST:
+            return {
+                ...state,
+                isFetching: true, responseCode: null
+            };
+        case types.COURSES.SUCCESS:
+            return {
+                ...state,
+                isFetching: false, responseCode: action.responseCode,
+                results: action.courses
+            };
+
+        case types.COURSES.FAILURE:
+            return {
+                ...state,
+                isFetching: false, responseCode: action.errorCode
+            }
+    }
+    return state;
 }
 
 export default notes
