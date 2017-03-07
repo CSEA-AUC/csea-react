c3dc4b3e62fbaf82527769f03077b99d3eb30d38
ShadyF <shadster09@gmail.com>
2017-03-07 21:29:26 +0200

    Added validators to createGroupForm

--- src/components/alexa/create-group-form/create-group-form.js
+++ src/components/alexa/create-group-form/create-group-form.js
@@ -1,7 +1,55 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
-import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
+import {FormGroup, FormControl, ControlLabel, InputGroup, Button} from 'react-bootstrap'
 
+const required = value => value ? undefined : 'Required';
+const maxLength = max => value =>
+    value && value.length > max ? `Must be ${max} characters or less` : undefined;
+const maxLength50 = maxLength(50);
+const minLength = min => value =>
+    value && value.length < min ? `Must be ${min} characters or less` : undefined;
+const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined;
+const minValue = min => value =>
+    value && value < min ? `Must be at least ${min}` : undefined;
+const minValue3 = minValue(3);
+const aucEmail = value =>
+    value && !/^[A-Z0-9._%+-]+@$/i.test(value) ? 'Invalid email address' : undefined;
+const mobileNumber = number =>
+    number && !/^\+?1?\d{9,11}$/i.test(number) ? 'Invalid mobile number' : undefined;
+
+class AUCEmailInput extends Component {
+    render() {
+        const {
+            feedbackIcon,
+            input,
+            label,
+            type,
+            meta: {error, warning, touched},
+            ...props
+        } = this.props;
+
+        let message;
+        const validationState = touched && ( error && "error" ) || ( warning && "warning" ) || null;
+
+        if (touched && ( error || warning )) {
+            message = <span className="help-block">{ error || warning }</span>;
+        }
+
+        return (
+            <FormGroup validationState={ validationState }>
+                <ControlLabel>{ label }</ControlLabel>
+                <InputGroup>
+                    <FormControl { ...input }
+                                 type={ type }
+                                 { ...props }/>
+                    <InputGroup.Addon>@aucegypt.edu</InputGroup.Addon>
+                </InputGroup>
+                { feedbackIcon ? <FormControl.Feedback>{ feedbackIcon }</FormControl.Feedback> : null }
+                { message }
+            </FormGroup>
+        );
+    }
+}
 class Input extends Component {
     render() {
         const {
@@ -37,17 +85,32 @@ class Input extends Component {
 
 class CreateGroupForm extends Component {
     render() {
-        const {handleSubmit} = this.props;
+        const {handleSubmit, pristine, reset, submitting, valid} = this.props;
+        console.log(this.props);
         return (
             <form onSubmit={handleSubmit}>
                 <div>
-                    <Field name="name" component={Input} label="Full Name"/>
+                    <Field name="name" component={Input} label="Full Name" validate={[required, maxLength50, minValue3]}/>
                 </div>
                 <div>
-
+                    <Field name="aucEmail" component={AUCEmailInput} label="AUC Email"/>
                 </div>
                 <div>
-                    <button type="submit">Submit</button>
+                    <Field name="mobile" component={Input} label="Mobile Number" type="number"/>
+                </div>
+                <div>
+                    <Field name="major" component={Input} label="Major"/>
+                </div>
+                <div>
+                    <Field name="standing" component={Input} label="Standing" componentClass="select">
+                        <option value="freshman">Freshman</option>
+                        <option value="sophomore">Sophomore</option>
+                        <option value="junior">Junior</option>
+                        <option value="senior">Senior</option>
+                    </Field>
+                </div>
+                <div>
+                    <Button type="submit" bsStyle="primary" disabled={submitting || !valid}>Submit</Button>
                 </div>
             </form>
         )
@@ -56,7 +119,7 @@ class CreateGroupForm extends Component {
 
 // Decorate the form component
 CreateGroupForm = reduxForm({
-    form: 'createGroup' // a unique name for this form
+    form: 'createGroup',
 })(CreateGroupForm);
 
 export default CreateGroupForm;
